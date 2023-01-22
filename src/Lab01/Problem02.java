b363b862ecf7b8acb1c97a43d829fb62e8053305
Subata Naveen Khan <s.khan_18119@khi.iba.edu.pk>
2023-01-22 22:08:11 +0500

    added problem 2

--- src/Lab01/Problem02.java
+++ src/Lab01/Problem02.java
@@ -2,18 +2,31 @@ package Lab01;
 
 public class Problem02 {
     public static void main(String[] args) {
-        String orig = "aabababacdefaa";
-        String want = "bacd";
-
-        int index = locate(orig, want);
-        System.out.println(index);
+        String original = "bananas";
+        String find = "anas";
+        FindIndex(original, find);
     }
-    
-    private static int locate(String string, String substring) {
-        for (int i = 0; i < string.length() - (substring.length() - 1); i++) {
-            
+
+    public static int FindIndex(String original, String find) {
+        int index_found = 0;
+        boolean match_found = false;
+        for (int i = 0; i < original.length(); i++) {
+            String store = "";
+            for (int x = i; x < i + find.length(); x++) {
+                store += original.charAt(x);
+            }
+            if (find.compareTo(store) == 0) {
+                index_found = i;
+                match_found = true;
+                break;
+            }
         }
-        
-        return -1;
+        if (match_found) {
+            System.out.println("match found at index " + index_found);
+        } else {
+            System.out.println("no match");
+            index_found = -1;
+        }
+        return index_found;
     }
 }
