7ce33c6b2edee9226034ddb26f5585d9e01cc600
Subata Naveen Khan <s.khan_18119@khi.iba.edu.pk>
2023-02-17 09:20:45 +0500

    lab 05

---
+++ src/Lab05/LinkedListManager.java
@@ -0,0 +1,27 @@
+package Lab05;
+// done by ali ahmed
+
+public class LinkedListManager {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.append(10);
+        list.append(10);
+        list.append(10);
+        list.append(5);
+        list.append(1, 0);
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        list.removeFromIndex(2);
+        list.remove(5);
+        list.removeAllOccurrences(10);
+
+        String text = "This is some text that will be converted to ASCII values";
+
+        Problem3.storeStringAscii(text, list);
+        System.out.println("ASCII values in list: " + list);
+        String out = Problem4.combineAscii(list);
+        System.out.println(out);
+    }
+}
